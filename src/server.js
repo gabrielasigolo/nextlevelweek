0d538b0656307cf19b075a6a7dde330348e94292
jhonathannc <jhonathannc@live.com>
2020-08-06 21:23:20 -0300

    corrigido give-classes

--- src/server.js
+++ src/server.js
@@ -1,43 +1,37 @@
 // Dados
-const proffys = [
-    {   
+const proffys = [{
         name: "Diego Fernandes",
-        avatar:"https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4", 
-        whatsapp:"89987654534", 
-        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.", 
-        subject:"Química",
-        cost:"20",
-        weekday:[0], 
-        time_from:[720], 
-        time_to:[1220]
+        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
+        whatsapp: "89987654534",
+        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+        subject: "Química",
+        cost: "20",
+        weekday: [0],
+        time_from: [720],
+        time_to: [1220]
     },
-    {  
+    {
         name: "Daniele Evangelista",
-        avatar:"https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4", 
-        whatsapp:"89987654534", 
-        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.", 
-        subject:"Química",
-        cost:"20",
-        weekday:[0], 
-        time_from:[720], 
-        time_to:[1220]
+        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
+        whatsapp: "89987654534",
+        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+        subject: "Química",
+        cost: "20",
+        weekday: [0],
+        time_from: [720],
+        time_to: [1220]
     },
-    {  
+    {
         name: "Mayk Brito",
-        avatar:"https://avatars2.githubusercontent.com/u/6643122?s=460&u=1e9e1f04b76fb5374e6a041f5e41dce83f3b5d92&v=4", 
-        whatsapp:"89987654534", 
-        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.", 
-        subject:"Química",
-        cost:"20",
-        weekday:[0], 
-        time_from:[720], 
-        time_to:[1220]
+        avatar: "https://avatars2.githubusercontent.com/u/6643122?s=460&u=1e9e1f04b76fb5374e6a041f5e41dce83f3b5d92&v=4",
+        whatsapp: "89987654534",
+        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+        subject: "Química",
+        cost: "20",
+        weekday: [0],
+        time_from: [720],
+        time_to: [1220]
     }
-
-
-
- 
-
 ]
 
 const subjects = [
@@ -69,33 +63,33 @@ function pageLanding(req, res) {
 }
 
 function pageStudy(req, res) {
-   const filters = req.query
+    const filters = req.query
     return res.render("study.html", { proffys, filters, subjects, weekdays })
 }
 
 function pageGiveClasses(req, res) {
-    return res.render("give-classes.html", {subjects, weekdays})
+    return res.render('give-classes.html', { subjects, weekdays })
 }
 
 
 //Servidor
 const express = require('express')
 const server = express()
-const nunjucks = require("nunjucks")
+const nunjucks = require('nunjucks')
 
 // configurar nunjucks (template engine)
 nunjucks.configure('src/views', {
     express: server,
-    noCache: true,
+    noCache: true
 })
 
 //Inicio e configuração do servidor 
 server
 // configurar arquivos estáticos (css, scripts, imagens)
-.use(express.static("public"))
-// rotas da aplicação
-.get("/", pageLanding)
-.get("/study", pageStudy)
-.get("/give-classes", pageGiveClasses)
-// start do servidor 
-.listen(5500)
+    .use(express.static('public'))
+    // rotas da aplicação
+    .get('/', pageLanding)
+    .get('/study', pageStudy)
+    .get('/give-classes', pageGiveClasses)
+    // start do servidor 
+    .listen(5500)
